12eae80d003ce026b13dfae4200d18ffbf9eabba
tarasVarvariuk <taras20109@gmail.com>
2016-03-24 16:34:52 +0200

    added build project

---
+++ EasyTour/src/main/java/com/java/tour_firm/App.java
@@ -0,0 +1,13 @@
+package com.java.tour_firm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
