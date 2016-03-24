4845b191f6141b1fb20cc6523aaed99e25d083df
vterlyha <vterlyha@gmail.com>
2016-03-25 00:45:09 +0200

    Entity classes Booking, Visa, Client and Adress added

---
+++ EasyTourJPA/src/main/java/entity/City.java
@@ -0,0 +1,55 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cities")
+public class City {
+	
+	@Id @GeneratedValue
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "countryId")
+	private Country country;
+
+	public City() {}
+	
+	public City(Integer id, String name, Country country) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.country = country;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+}
---
+++ EasyTourJPA/src/main/java/entity/HotelCategory.java
@@ -0,0 +1,43 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "hotelCategory")
+public class HotelCategory {
+
+	@Id @GeneratedValue
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "category")
+	private String category;
+
+	public HotelCategory() {}
+	
+	public HotelCategory(Integer id, String category) {
+		super();
+		this.id = id;
+		this.category = category;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+}
